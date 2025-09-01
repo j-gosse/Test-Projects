d5fd2a0b3e90cfdb875303316e5cc812f3951117
j-gosse <65306279+j-gosse@users.noreply.github.com>
2025-09-01 07:16:07 -0400

    Minor edit to copyright notice

--- WinAPI_CreateWindow/WinAPI_CreateWindow/main.cpp
+++ WinAPI_CreateWindow/WinAPI_CreateWindow/main.cpp
@@ -7,7 +7,7 @@
     \MSVC       /std:c++20
     \GNUC       -m64 -std=c++20
 
-   Copyright 2025 Jacob Gosse
+   Copyright (c) 2025, Jacob Gosse
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
