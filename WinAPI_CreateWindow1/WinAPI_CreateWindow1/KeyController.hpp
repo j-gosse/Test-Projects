782f1e25a7fae28fa1193fae36db09807ef85c87
j-gosse <65306279+j-gosse@users.noreply.github.com>
2025-09-06 16:13:36 -0400

    WinAPI_CreateWindow1 project v1.0 Inclusion of key listener/handler

---
+++ WinAPI_CreateWindow1/WinAPI_CreateWindow1/KeyController.hpp
@@ -0,0 +1,55 @@
+// KeyController.hpp : example of how the KeyListener and KeyHandler classes can be utilized 
+// in conjunction with Window messages in order to listen for key presses from any class
+// 
+
+#pragma once
+
+#ifndef KEYCONTROLLER_HPP_
+#define KEYCONTROLLER_HPP_
+
+#include "IKeyListener.hpp"
+
+class KeyController : public IKeyListener
+{
+public:
+    KeyController() { std::wcout << L"CONSTRUCTOR: KeyController()" << '\n'; }
+    virtual ~KeyController() { std::wcout << L"DESTRUCTOR: ~KeyController()" << '\n'; }
+
+    void OnKeyDown(WPARAM key) override
+    {
+        std::wcout << L"Key Down: " << key << "\n";
+
+        switch (key)
+        {
+        case 'W':
+            std::cout << "Pressed W\n";
+            break;
+        case 'A':
+            std::cout << "Pressed A\n";
+            break;
+        case 'S':
+            std::cout << "Pressed S\n";
+            break;
+        case 'D':
+            std::cout << "Pressed D\n";
+            break;
+        case VK_SPACE:
+            std::cout << "Pressed Spacebar\n";
+            break;
+        default:
+            break;
+        }
+    }
+
+    void OnKeyUp(WPARAM key) override
+    {
+        std::wcout << L"Key Up: " << key << "\n";
+    }
+
+    void OnChar(WPARAM ch) override
+    {
+        std::wcout << L"Char: " << (char)ch << "\n";
+    }
+};
+
+#endif
